d5058b7068c7522d48e8af77f613f67d20968f2a
Ravi Anugu <ravi.rgu09@gmail.com>
2016-12-01 23:11:13 +0530

    Add files via upload
    JavaScript Variables

---
+++ Assignment5.1/js/app.js
@@ -0,0 +1,9 @@
+var name = "Ravi Anugu";
+var age = 23;
+var dateOfBirth = "10-Sep-1993";
+var placeOfBirth = "Nalgonda";
+
+console.log("Name: ",  name);
+console.log("Age: ", age);
+console.log("Date Of Birth: ", dateOfBirth);
+console.log("Place OF Birth: ", placeOfBirth);
